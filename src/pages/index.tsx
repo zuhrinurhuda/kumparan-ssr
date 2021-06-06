270727700c6d3cedf59a8d223f90bac77f8297a6
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2021-06-06 16:10:53 +0700

    Add user list to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import { useSelector, useStore } from 'react-redux'
 import { wrapper } from '../app/store'
 import { fetchUserListAction } from '../app/users/action'
 import { selectUserList } from '../app/users/selector'
-import styles from '../styles/Home.module.css'
 
 const IndexPage: NextPage = (props) => {
   console.log('State on render', useStore().getState(), props)
   const userList = useSelector(selectUserList)
-  console.log('userList', userList)
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Redux Toolkit</title>
+        <title>Home page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <img src="/logo.svg" className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <header></header>
+      <main>
+        {userList.map((user) => {
+          return (
+            <Link key={user.id} as={`/users/${user.id}`} href="/users/[userId]">
+              <div>
+                <p>{`@${user.username}`}</p>
+                <ul>
+                  <li>{`name: ${user.name}`}</li>
+                  <li>{`email: ${user.email}`}</li>
+                  <li>{`phone: ${user.phone}`}</li>
+                  <li>{`website: ${user.website}`}</li>
+                </ul>
+              </div>
+            </Link>
+          )
+        })}
+      </main>
     </div>
   )
 }
